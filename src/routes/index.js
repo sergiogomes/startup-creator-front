1302942347200e36286841c74b031de970a011eb
Janley Santos <janley.santos@gmail.com>
2021-10-19 09:35:41 -0300

    Arrumando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,9 +11,9 @@ import Rank from '../pages/Rank';
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Prova} />
-    <Route path="/eutonalive" exact component={EuToNaLive} />
-    <Route path="/ip" exact component={AutoGoverno127} />
-    <Route path="/rank" exact component={Rank} />
+    <Route path="/eutonalive" component={EuToNaLive} />
+    <Route path="/ip" component={AutoGoverno127} />
+    <Route path="/rank" component={Rank} />
   </Switch>
 );
 
