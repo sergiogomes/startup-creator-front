20e0d92974273e1cc289d4f0e7a5f8dae77de1a1
Janley Santos <janley.santos@gmail.com>
2021-10-21 13:25:00 -0300

    Criando o rank da turma do ip-127

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import EuToNaLive from '../pages/EuToNaLive';
 import AutoGoverno127 from '../pages/AutoGoverno127';
+import TurmaIp127 from '../pages/TurmaIp127';
 import Prova from '../pages/Prova';
 import Rank from '../pages/Rank';
 
@@ -13,6 +14,7 @@ const Routes = () => (
     <Route path="/" exact component={Prova} />
     <Route path="/eutonalive" component={EuToNaLive} />
     <Route path="/ip" component={AutoGoverno127} />
+    <Route path="/ip-127" component={TurmaIp127} />
     <Route path="/rank" component={Rank} />
   </Switch>
 );
