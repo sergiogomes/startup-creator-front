1d377b9d71a83889f30303e96a69faeebe74c3c6
Janley Santos <janley.santos@gmail.com>
2021-12-21 20:32:04 -0300

    Ajuste de layout das caronas

--- src/pages/Caronas.js
+++ src/pages/Caronas.js
@@ -23,7 +23,7 @@ const Caronas = () => {
   const [perguntas, setPerguntas] = useState([]);
   const [respostas, setRespostas] = useState([]);
   const [prova, setProva] = useState('');
-  const [isAluno, setIsAluno] = useState(false);
+  const [isAluno, setIsAluno] = useState(true);
   const [aluno, setAluno] = useState({});
   const [isProvaRespondida, setIsProvaRespondida] = useState(false);
   const [pontos, setPontos] = useState(0);
@@ -180,7 +180,11 @@ const Caronas = () => {
   }
 
   const abrirLink = () => {
-    window.open("https://devzap.com.br/api-engennier/campanha/api/redirect/60888eb7001c7c0001c17933");
+    window.open("http://plataformagame.com.br/caronas-tanques");
+  }
+
+  const abrirCadastro = () => {
+    setIsAluno(false);
   }
 
   const handleSubmit = (event) => {
@@ -194,7 +198,7 @@ const Caronas = () => {
   return (
     <Container className="p-3" style={{ background: '#000' }}>
       <div className="centerImg">
-        <img src={"img/logo-caronas.png"} alt="O Chamado 2022" height="220"/>
+        <img src={"img/logo-caronas.png"} alt="O Chamado 2022" height="130"/>
       </div>
       {!isAluno ? (
         <Jumbotron className="painel" style={{ background: '#1a1a1a' }}>
@@ -283,178 +287,67 @@ const Caronas = () => {
           </Form>
         </Jumbotron>
       ) : (
-        <>
-        {!isProvaRespondida ? (
-          <>
-            <h1 className="nota center">CARONAS - O CHAMADO DOS GENERAIS!</h1>
-          {perguntas.map((pergunta, i) => {
-            return (
-              <>
-                <Jumbotron className="painel" id={`pergunta-${pergunta.id}`} style={{ background: '#1a1a1a', display: 'none' }}>
-                  <Form style={{ background: '#1a1a1a' }} onSubmit={handleSubmit}>
-                  <h1 className="pergunta">{`${i + 1}) ${pergunta.pergunta}`}</h1>
-                  <fieldset className="alternativasRadius">
-                    <Form.Group as={Row} className="mb-3">
-                        <Col sm={10}>
-                          {respostas.map((resposta, i) => {
-                            if (pergunta.id !== resposta.perguntas_id) {
-                              return <></>;
-                            }
-                            return (
-                              <>
-                                <Form.Check
-                                style={{ color: '#fff' }}
-                                type="radio"
-                                key={i}
-                                label={`${resposta.opcao}) ${resposta.resposta}`}
-                                onChange={salvarResposta}
-                                name={`${pergunta.id}`}
-                                value={`${resposta.id}`}
-                                opcao={`${resposta.opcao}`}
-                                id={`formHorizontalRadios${resposta.id}`}
-                                className="alternativa" />
-                              </>
-                            )
-                          })}
-                        </Col>
-                      </Form.Group>
-                    </fieldset>
-                  </Form>
-                </Jumbotron>
-              </>
-              )
-            })}
-            {/* <div className="center">
-              <Form.Group as={Row} className="mb-3">
-                <Button type="button" className="btnEnviarRespostas" onClick={enviarResposta}>ENVIAR&nbsp;RESPOSTAS</Button>
-              </Form.Group>
-            </div> */}
-            <h1 className="nota center">Você não vai chegar até o final.</h1>
-            <h1 className="nota center"></h1>
-
-          </>
-        ) : (
-          <Jumbotron className="painel" style={{ background: '#1a1a1a' }}>
-            <div className="">
-              <h1 className="pergunta center tituloResultado">TESTE CONCLUÍDO</h1>
-              <br />
-              <h1 className="pergunta center">VOCÊ ESTÁ NA FASE: </h1>
-              <h1 className="pergunta center perfil">{`${perfilSelecionado}`}</h1>
-
-              {/* {perfilSelecionado === 'ZUMBI' ? (
-                <Iframe url="http://www.youtube.com/embed/84iHW5laImM"
-                  width="100%"
-                  height="450px"
-                  display="initial"
-                  position="relative"
-                />
-              ) : (
-                <></>
-              )}
-
-              {perfilSelecionado === 'ESCRAVO' ? (
-                <Iframe url="http://www.youtube.com/embed/84iHW5laImM"
-                  width="100%"
-                  height="450px"
-                  display="initial"
-                  position="relative"
-                />
-              ) : (
-                <></>
-              )}
-
-              {perfilSelecionado === 'ESCASSO' ? (
-                <Iframe url="http://www.youtube.com/embed/84iHW5laImM"
-                  width="100%"
-                  height="450px"
-                  display="initial"
-                  position="relative"
-                />
-              ) : (
-                <></>
-              )}
-
-              {perfilSelecionado === 'ABUNDANTE' ? (
-                <Iframe url="http://www.youtube.com/embed/84iHW5laImM"
-                  width="100%"
-                  height="450px"
-                  display="initial"
-                  position="relative"
-                />
-              ) : (
-                <></>
-              )}
-
-              {perfilSelecionado === 'TRANSBORDANTE' ? (
-                <Iframe url="http://www.youtube.com/embed/84iHW5laImM"
-                  width="100%"
-                  height="450px"
-                  display="initial"
-                  position="relative"
-                />
-              ) : (
-                <></>
-              )} */}
-
-              <br />
-              <br />
-              <div className="center">
-                <Form.Group as={Row} className="mb-3">
-                  <Button className="btnEnviarRespostasWhats" onClick={abrirLink}>
-                    <img className="logoBotaoWhats" src={"img/whatsapp-branco.png"} />
-                    &nbsp;QUERO RECEBER OS CONTEÚDOS
-                  </Button>
-                  <ToastContainer />
-                </Form.Group>
-              </div>
-              
-              <h1 className="pergunta center">Grupo Fechado e Silencioso</h1>
-            </div>
-
-          </Jumbotron>
-        )}
-        </>
+        <Jumbotron className="painel" style={{ background: '#1a1a1a' }}>
+          <h1 className="pergunta center tituloGenerais">
+            Convocação dos Generais 
+          </h1>
+          <br/>
+          <h1 className="perguntaGeneral center">
+            Encontre sua carona para “O Chamado”
+            29 de dezembro - Goiânia
+          </h1>
+          <br/>
+          <h1 className="perguntaGeneral center" style={{ fontWeight: 100 }}>
+            Abaixo você tem duas opções: Dar carona para algum general,
+            ou encontrar um batalhão que possa te levar ao evento. 
+          </h1>
+          <br/>
+          <h1 className="perguntaGeneral center">
+            Clique no botão abaixo e bora tocar o terror!
+          </h1>
+          <br/>
+          <div className="center">
+            <Form.Group as={Row} className="mb-3" style={{ width: '100%' }}>
+              <Button
+                type="submit"
+                className="btnEnviarRespostas"
+                onClick={abrirCadastro}
+                style={{ 
+                  background: '#76793F',
+                  borderColor: '#76793F',
+                  width: '100%'
+                }}
+              >
+                TENHO CARRO E QUERO DAR CARONA
+              </Button>
+              <ToastContainer />
+            </Form.Group>
+          </div>
+          <h1 className="perguntaGeneral center">
+            OU
+          </h1>
+          <div className="center">
+            <Form.Group as={Row} className="mb-3" style={{ width: '100%' }}>
+              <Button
+                type="submit"
+                className="btnEnviarRespostas"
+                onClick={abrirLink}
+                style={{ 
+                  background: '#76793F',
+                  borderColor: '#76793F',
+                  width: '100%'
+                }}
+              >
+                PRECISO DE CARONA
+              </Button>
+              <ToastContainer />
+            </Form.Group>
+          </div>
+        </Jumbotron>
       )}
       <div className="centerImg">
         <img className="logoRotaDigital" src={"img/pablo1.png"} alt="Pablo Marçal"/>
       </div>
-
-      <Helmet>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-455561195"></script>
-        <script>
-          {`
-            if (document.location.pathname.indexOf("/pior-semana-da-sua-vida-quizz") == 0) {
-              console.log('pior-semana-da-sua-vida-quizz');
-
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '608209829624734');
-              fbq('track', 'PageView');
-              fbq('track', 'Lead');
-
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'AW-480840146');
-
-              gtag('event', 'conversion', {'send_to': 'AW-480840146/-im6CMfzzoADENKTpOUB'});
-            }
-          `}
-        </script>
-      </Helmet>
-
-      <noscript>
-        <img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=608209829624734&ev=PageView&noscript=1"
-        />
-      </noscript>
     </Container>
   );
 };
