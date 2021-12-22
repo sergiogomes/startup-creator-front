5383440622bdf08d8bfc76316b8bf9a80c28b69f
Janley Santos <janley.santos@gmail.com>
2021-12-22 13:23:53 -0300

    Formatar telefone

--- src/pages/Caronas.js
+++ src/pages/Caronas.js
@@ -146,6 +146,39 @@ const Caronas = () => {
     event.preventDefault();
   }
 
+  const mask = (event) => {
+    let numero = event.target.value;
+    console.log(numero);
+    setTimeout(() => {
+        let v = mphone(numero);
+        if (v != numero) {
+          event.target.value = v;
+        }
+    }, 1);
+  }
+  
+  const mphone = (v) => {
+    var r = v.replace(/\D/g,"");
+    r = r.replace(/^0/,"");
+    if (r.length > 10) {
+        // 11+ digits. Format as 5+4.
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/,"($1) $2-$3");
+    }
+    else if (r.length > 5) {
+        // 6..10 digits. Format as 4+4
+        r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/,"($1) $2-$3");
+    }
+    else if (r.length > 2) {
+        // 3..5 digits. Add (0XX..)
+        r = r.replace(/^(\d\d)(\d{0,5})/,"($1) $2");
+    }
+    else {
+        // 0..2 digits. Just add (0XX
+        r = r.replace(/^(\d*)/, "($1");
+    }
+    return r;
+  }
+
   useEffect(() => {
     // getProva();
   }, []);
@@ -188,6 +221,9 @@ const Caronas = () => {
                   placeholder="Seu Whatsapp com DDD"
                   name="whatsapp"
                   onChange={setDadosForm}
+                  onKeyPress={mask}
+                  onKeyUp={mask}
+                  onBlur={mask}
                 />
                 <Form.Control 
                   className="inputEmail"
