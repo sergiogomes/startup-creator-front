90206d4157810f1e8218e562118cbea5aeaabd74
Janley Santos Soares <janley.santos@gmail.com>
2022-02-25 21:58:34 -0300

    arrumando api

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3934'
-    : 'http://api.veloycard.com.br';
+    ? 'http://localhost:3334'
+    : 'http://api.plataformagame.com.br';
 
 const api = axios.create({ baseURL: url });
 
