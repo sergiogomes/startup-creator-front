9ce36c0ac88ac129096db80b0b6169305c96454a
Paulo Roberto <git@binovacao.com.br>
2022-08-11 14:52:17 +0000

    api na aws

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axiosRetry from 'axios-retry';
 const url =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3334'
-    : 'http://api.plataformagame.com.br';
+    : 'https://vkhdmal19b.execute-api.sa-east-1.amazonaws.com/prod';
 
 const api = axios.create({ baseURL: url });
 const shouldRetry = error => {
