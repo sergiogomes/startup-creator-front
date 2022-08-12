a29615d44d213c7f3626c9b5c1cebbf1c76caa63
Paulo Roberto <pfreitas@eshopworld.com>
2022-08-12 13:49:20 -0300

    fix pixel

--- src/pages/RotaDigitalProva.js
+++ src/pages/RotaDigitalProva.js
@@ -125,6 +125,8 @@ const RotaDigitalProva = () => {
      setPerfilSelecionado(retorno.data[0].categoria)
      setPerfilSelecionado2(retorno.data[1].categoria)
      setPerfilSelecionado3(retorno.data[2].categoria)
+     
+     
     } catch (error) {
       console.log(error);
     }
@@ -318,21 +320,8 @@ const RotaDigitalProva = () => {
         ) : (
           <Jumbotron className="painel painelresultado" style={{ background: '#1a1a1a' }}> 
 
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1154748838448553&ev=CompleteRegistration&noscript=1"/>
 
-<Script>
-  {`
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1154748838448553');
-fbq('track', 'CompleteRegistration');
-`}
-</Script>
 
 
             <h1 className="pergunta center tituloResultado">TESTE CONCLUÍDO</h1>
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axiosRetry from 'axios-retry';
 
 const url =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3334'
+    ? 'https://vkhdmal19b.execute-api.sa-east-1.amazonaws.com/prod'
     : 'https://vkhdmal19b.execute-api.sa-east-1.amazonaws.com/prod';
 
 const api = axios.create({ baseURL: url });
