d996cc8f5845d1c5bb1892ecebb1b14ad3516f1a
Janley Santos Soares <janleysantossoares@MacBook-Pro-de-Janley.local>
2022-10-12 12:56:59 -0300

    trocando url api

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axiosRetry from 'axios-retry';
 
 const url =
   process.env.NODE_ENV === 'development'
-    ? 'https://vkhdmal19b.execute-api.sa-east-1.amazonaws.com/prod'
-    : 'https://vkhdmal19b.execute-api.sa-east-1.amazonaws.com/prod';
+    ? 'https://fult5o9ub7.execute-api.sa-east-1.amazonaws.com/prod'
+    : 'https://fult5o9ub7.execute-api.sa-east-1.amazonaws.com/prod';
 
 const api = axios.create({ baseURL: url });
 const shouldRetry = error => {
